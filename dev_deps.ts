296e5d4c9404241bd1d09dec84ba44a6d6995185
Dan Rumney <dancrumb@gmail.com>
2023-06-04 13:47:06 -0500

    test(createSpanContext): add basic test for createSpanContext
    #2

---
+++ dev_deps.ts
@@ -0,0 +1,11 @@
+export {
+  assertEquals,
+  assertExists,
+} from "https://deno.land/std@0.177.1/testing/asserts.ts";
+export { describe, it } from "https://deno.land/std@0.189.0/testing/bdd.ts";
+export {
+  assertSpyCalls,
+  returnsNext,
+  spy,
+  stub,
+} from "https://deno.land/std@0.189.0/testing/mock.ts";
---
+++ span-context_test.ts
@@ -0,0 +1,24 @@
+import { TraceFlags, getEmptyTraceState } from "./deps.ts";
+import { assertEquals, assertExists, describe, it } from "./dev_deps.ts";
+import { createSpanContext } from "./span-context.ts";
+
+describe("createSpanContext", () => {
+  it("should create a span context", () => {
+    const spanContext = createSpanContext({
+      traceId: new Uint8Array(16),
+      spanId: new Uint8Array(8),
+      traceFlags: TraceFlags.NONE,
+      traceState: getEmptyTraceState(),
+      isRemote: false,
+    });
+    assertExists(spanContext);
+    assertEquals(spanContext.isValid, false);
+    assertEquals(spanContext.isRemote, false);
+    assertEquals(spanContext.traceFlags, TraceFlags.NONE);
+    assertEquals(
+      spanContext.getTraceId("hex"),
+      "00000000000000000000000000000000"
+    );
+    assertEquals(spanContext.getSpanId("hex"), "0000000000000000");
+  });
+});
